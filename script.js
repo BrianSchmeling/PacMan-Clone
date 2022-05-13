a0c194ccf1074a9b69b80fe6951ae11615944073
Brian Schmeling <brianschmeling2@gmail.com>
2022-05-13 15:33:56 -0400

    Create index.html style.css and script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+const width = canvas.width
+const height = canvas.height
+
+canvas.style.border = '1px black solid'
