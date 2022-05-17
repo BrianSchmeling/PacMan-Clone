9485d3d733583610f7a503934c9029281cecda4f
Brian Schmeling <brianschmeling2@gmail.com>
2022-05-17 14:04:50 -0400

    Refactor into classes, fix collision detection

--- script.js
+++ script.js
@@ -18,10 +18,66 @@ Keep Score
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+let playerDir = "";
+
+class Wall {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 50;
+    this.height = 50;
+  }
+  drawMap() {
+    ctx.beginPath();
+    ctx.rect(this.position.x, this.position.y, 50, 50);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+class Pellet {
+  constructor({ position }, color) {
+    this.position = position;
+    this.color = color;
+    this.size = 6;
+    this.value = 0;
+  }
+  drawPellets() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+class Player {
+  constructor({ position, speed }) {
+    this.position = position;
+    this.speed = speed;
+    this.size = 15;
+  }
+  drawPlayer() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.closePath();
+  }
+  move() {
+    this.drawPlayer();
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+  }
+}
 
 const map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
   [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
@@ -34,20 +90,60 @@ const map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-const mapArr = [];
+canvas.width = map.length * 50;
+canvas.height = map[0].length * 50;
 
-const tileSize = 50;
-canvas.height = map.length * tileSize;
-canvas.width = map[0].length * tileSize;
-let x = 0;
-let y = 0;
-let playerX = 475;
-let playerY = 75;
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
-let playerDir = "";
+const walls = [];
+const pellets = [];
+
+const player = new Player({
+  position: {
+    x: 75,
+    y: 75,
+  },
+  speed: {
+    x: 0,
+    y: 0,
+  },
+});
+
+for (i = 0; i < map.length; i++) {
+  for (j = 0; j < map[i].length; j++) {
+    if (map[i][j] === 1)
+      walls.push(
+        new Wall({
+          position: {
+            x: 50 * i,
+            y: 50 * j,
+          },
+        })
+      );
+  }
+}
+
+walls.forEach((wall) => {
+  wall.drawMap();
+});
+
+for (i = 0; i < map.length; i++) {
+  for (j = 0; j < map[i].length; j++) {
+    if (map[i][j] === 0)
+      pellets.push(
+        new Pellet({
+          position: {
+            x: 50 * i + 25,
+            y: 50 * j + 25,
+          },
+        })
+      );
+  }
+}
+
+// pellets.forEach((pellet) => {
+//   pellet.drawPellets("blue");
+// });
+
+player.drawPlayer();
 
 document.addEventListener("keydown", keyDown, false);
 document.addEventListener("keyup", keyUp, false);
@@ -55,171 +151,73 @@ document.addEventListener("keyup", keyUp, false);
 function keyDown(e) {
   if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = true;
-    playerDir = "right";
+    player.speed.x = 50;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = true;
-    playerDir = "left";
+    player.speed.x = -50;
   } else if (e.key == "Up" || e.key == "ArrowUp") {
     upPressed = true;
-    playerDir = "up";
+    player.speed.y = -50;
   } else if (e.key == "Down" || e.key == "ArrowDown") {
     downPressed = true;
-    playerDir = "down";
+    player.speed.y = 50;
   }
 }
 
 function keyUp(e) {
   if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = false;
+    player.speed.x = 0;
+    // player.move();
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
+    player.speed.x = 0;
+    // player.move();
   } else if (e.key == "Up" || e.key == "ArrowUp") {
     upPressed = false;
+    player.speed.y = 0;
+    // player.move();
   } else if (e.key == "Down" || e.key == "ArrowDown") {
     downPressed = false;
+    player.speed.y = 0;
+    // player.move();
   }
 }
 
-function drawMap() {
-  for (i = 0; i < map.length; i++) {
-    for (j = 0; j < map[i].length; j++) {
-      if (map[i][j] === 0) {
-        drawPath();
-      } else if (map[i][j] === 1) {
-        drawWall();
-      } else if (map[i][j] === 2) {
-        // clearPlayer();
-        drawPlayer();
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  walls.forEach((wall) => {
+    wall.drawMap();
+    pellets.forEach((pellet) => {
+      if (
+        player.position.x === pellet.position.x &&
+        player.position.y === pellet.position.y
+      ) {
+        pellet.value = 1;
+      } else if (pellet.value === 0) {
+        pellet.drawPellets();
       }
-      x += 50;
-    }
-    x = 0;
-    y += 50;
-  }
-  //   console.log(
-  //   );
-  if (rightPressed) {
-    //   if (playerDir === 'right' &&
-    // let corners =
-    //   "mapArr[" +
-    //   Math.floor(playerY / 50) +
-    //   "][" +
-    //   Math.floor((playerX - 25) / 50 + 1) +
-    //   "]";
+    });
     if (
-      playerDir === "right" &&
-      map[Math.floor(playerY / 50)][Math.floor((playerX - 25) / 50) + 1] !== 1
+      player.position.x + player.size >= wall.position.x &&
+      player.position.x - player.size <= wall.position.x + wall.width &&
+      player.position.y + player.size >= wall.position.y &&
+      player.position.y - player.size <= wall.position.y + wall.height
     ) {
-      //   (playerY + 25 && playerX + 26) <
-      //   corners.x(playerY + 25 && playerX - 26) <
-      //   corners.y
-      // )
-      //   console.log(corners);
-      // checkCollision();
-      // console.log(checkCollision());
-      playerX += 10;
-    }
-  } else if (leftPressed) {
-    if (
-      playerDir === "left" &&
-      map[Math.floor(playerY / 50)][Math.floor((playerX + 15) / 50) - 1] !== 1
-    ) {
-      playerX -= 10;
-    }
-  } else if (upPressed) {
-    if (
-      playerDir === "up" &&
-      map[Math.floor((playerY + 15) / 50) - 1][Math.floor(playerX / 50)] !== 1
-    ) {
-      playerY -= 10;
-    }
-  } else if (downPressed) {
-    if (
-      playerDir === "down" &&
-      map[Math.floor((playerY - 25) / 50) + 1][Math.floor(playerX / 50)] !== 1
-    ) {
-      playerY += 10;
-    }
-  }
-}
-
-function drawPath() {
-  ctx.beginPath();
-  ctx.rect(x, y, tileSize, tileSize);
-  ctx.fillStyle = "white";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawWall() {
-  ctx.beginPath();
-  ctx.rect(x, y, tileSize, tileSize);
-  ctx.fillStyle = "black";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawPlayer() {
-  ctx.beginPath();
-  ctx.rect(playerX - 25, playerY - 25, tileSize, tileSize);
-  ctx.fillStyle = "white";
-  ctx.fill();
-  ctx.closePath();
-  ctx.beginPath();
-  ctx.arc(playerX, playerY, 15, 0, Math.PI * 2);
-  ctx.fillStyle = "yellow";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function makeMapArr() {
-  for (i = 0; i < map.length; i++) {
-    for (j = 0; j < map[i].length; j++) {
-      if (map[i][j] == 1) {
-        mapArr.push({ x: i * 50, y: j * 50 });
+      if (rightPressed === true) {
+        player.speed.x = -50;
+      } else if (leftPressed === true) {
+        player.speed.x = 50;
+      } else if (upPressed === true) {
+        player.speed.y = 50;
+      } else if (downPressed === true) {
+        player.speed.y = -50;
       }
-    }
-  }
-}
-
-makeMapArr();
-
-// function moveX(e) {
-//   let start = Date.now;
-//   let timer = setInterval(function () {
-//     let timePassed = Date.now() - start;
-//     if (timePassed > 1000) {
-//       clearInterval(timer);
-//       return;
-//     }
-//     playerX += e;
-//   });
-// }
-
-function checkCollision() {
-  mapArr.forEach((map) => {
-    // console.log(map);
-    if (
-      playerX < map.x + tileSize &&
-      playerX + tileSize > map.x &&
-      playerY < map.y + tileSize &&
-      playerY + tileSize > map.y
-    ) {
-      return false;
-    } else {
-      return true;
     }
   });
+  player.move();
+  player.speed.x = 0;
+  player.speed.y = 0;
 }
-
-checkCollision();
-
-// function clearPlayer() {
-//   ctx.beginPath();
-//   ctx.rect(x, y, tileSize, tileSize);
-//   ctx.fillStyle = "white";
-//   ctx.fill();
-//   ctx.closePath();
-// }
-
-setInterval(drawMap, 1000 / 40);
+animate();
